ec5de684b39fd127510f96ad2886ce16f7fd1ee6
fuhuan1991 <huanfu2019@gmail.com>
2020-05-19 12:46:04 -0400

    add docs

---
+++ docs/precache-manifest.4685586be94ef582123851563ea2159f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "52df464201816fcc9491",
+    "url": "/static/css/main.a7e0a4da.chunk.css"
+  },
+  {
+    "revision": "52df464201816fcc9491",
+    "url": "/static/js/main.52df4642.chunk.js"
+  },
+  {
+    "revision": "49d26cbf72d1d80145d6",
+    "url": "/static/css/1.78f8f860.chunk.css"
+  },
+  {
+    "revision": "49d26cbf72d1d80145d6",
+    "url": "/static/js/1.49d26cbf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "53cd660d4a99d7add008354d13308358",
+    "url": "/static/media/wood.53cd660d.jpg"
+  },
+  {
+    "revision": "457cc00d385f2493561bbda5c3650233",
+    "url": "/static/media/t.457cc00d.png"
+  },
+  {
+    "revision": "6fece29beb44f127ef1c0dd6828e4761",
+    "url": "/static/media/h.6fece29b.png"
+  },
+  {
+    "revision": "12a428ac705534bdf0b9ba18ea65315d",
+    "url": "/index.html"
+  }
+];
